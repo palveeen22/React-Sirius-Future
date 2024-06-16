686cbf6f7cb5749c8258fe702b974c4bf4078827
Alvin Pratama <palveeen22@gmail.com>
2024-06-16 09:37:49 +0700

    feat(client): added setup config i18n lang

---
+++ src/i18n.ts
@@ -0,0 +1,6 @@
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+i18n.use(initReactI18next).init({
+ resources: {}, // Where we're gonna put translations' files
+ lng: "en",     // Set the initial language of the App
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import BaseLayout from "../layouts/BaseLayout";
 import LoginPage from "../pages/login/LoginPage";
 import CalendarPage from "../pages/CalendarPage";
 import HomePage from "../pages/HomePage";
+import NoAccess from "../components/NoAccess";
 
 
 export const router = createBrowserRouter([{
@@ -11,13 +12,25 @@ export const router = createBrowserRouter([{
         {
             path: "/",
             element: <LoginPage />,
-        },
-        {
-            path: "login",
-            element: <LoginPage />,
+            loader: () => {
+                if (localStorage.access_token) {
+                    <NoAccess />
+                    return redirect("/Home");
+                }
+
+                return null;
+            },
         },
         {
             element: <BaseLayout />,
+            loader: () => {
+                if (!localStorage.access_token) {
+                    <NoAccess />
+                    return redirect("/login");
+                }
+
+                return null;
+            },
             children: [
                 {
 
